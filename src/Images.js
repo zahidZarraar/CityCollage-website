fff23b224f57d65157e8f1d61ee239487589086a
spider076 <saadabban76@gmail.com>
2025-04-03 13:06:18 +0000

    added images in slider

--- src/Images.js
+++ src/Images.js
@@ -1,36 +1,54 @@
 // Slider Images
-import ai from './sliderImages/ai.jpg';
-import ce from './sliderImages/ce.jpg';
-import collage from './sliderImages/collage.jpg';
-import group from './sliderImages/group.jpg';
-import students from './sliderImages/students.jpg';
+import ai from "./sliderImages/ai.jpg";
+import ce from "./sliderImages/ce.jpg";
+import collage from "./sliderImages/collage.jpg";
+import group from "./sliderImages/group.jpg";
+import students from "./sliderImages/students.jpg";
+import one from "./sliderImages/one.jpg";
+import two from "./sliderImages/two.jpg";
+import three from "./sliderImages/three.jpg";
 // Company Logos
-import p1 from './placementImages/p1.png';
-import p2 from './placementImages/p2.png';
-import p3 from './placementImages/p3.png';
-import p4 from './placementImages/p4.png';
-import p5 from './placementImages/p5.png';
-import p6 from './placementImages/p6.png';
-import p7 from './placementImages/p7.png';
-import p8 from './placementImages/p8.png';
-import p9 from './placementImages/p9.png';
-import p10 from './placementImages/p10.png';
-import p11 from './placementImages/p11.png';
-import p12 from './placementImages/p12.png';
-import p13 from './placementImages/p13.png';
-import p14 from './placementImages/p14.png';
-import p15 from './placementImages/p15.png';
+import p1 from "./placementImages/p1.png";
+import p2 from "./placementImages/p2.png";
+import p3 from "./placementImages/p3.png";
+import p4 from "./placementImages/p4.png";
+import p5 from "./placementImages/p5.png";
+import p6 from "./placementImages/p6.png";
+import p7 from "./placementImages/p7.png";
+import p8 from "./placementImages/p8.png";
+import p9 from "./placementImages/p9.png";
+import p10 from "./placementImages/p10.png";
+import p11 from "./placementImages/p11.png";
+import p12 from "./placementImages/p12.png";
+import p13 from "./placementImages/p13.png";
+import p14 from "./placementImages/p14.png";
+import p15 from "./placementImages/p15.png";
 // NE Images
-import ne1 from './NEImages/ne1.jpg';
-import ne2 from './NEImages/ne2.jpg';
-import ne3 from './NEImages/ne3.jpg';
-import ne4 from './NEImages/ne4.jpg';
-import ne5 from './NEImages/ne5.jpg';
-import ne6 from './NEImages/ne6.jpg';
-import ne7 from './NEImages/ne7.jpg';
-import ne8 from './NEImages/ne8.jpg';
+import ne1 from "./NEImages/ne1.jpg";
+import ne2 from "./NEImages/ne2.jpg";
+import ne3 from "./NEImages/ne3.jpg";
+import ne4 from "./NEImages/ne4.jpg";
+import ne5 from "./NEImages/ne5.jpg";
+import ne6 from "./NEImages/ne6.jpg";
+import ne7 from "./NEImages/ne7.jpg";
+import ne8 from "./NEImages/ne8.jpg";
 
-
-export const SliderImages = [ai, ce, collage, group, students];
-export const placementImages = [p1,p2,p3,p4,p5,p6,p7,p8,p9,p10,p11,p12,p13,p14,p15];
-export const NeImages = [ne1,ne2,ne3,ne4,ne5,ne6,ne7,ne8];
+export const SliderImages = [ai, ce, collage, group, students, one, two, three];
+export const placementImages = [
+  p1,
+  p2,
+  p3,
+  p4,
+  p5,
+  p6,
+  p7,
+  p8,
+  p9,
+  p10,
+  p11,
+  p12,
+  p13,
+  p14,
+  p15,
+];
+export const NeImages = [ne1, ne2, ne3, ne4, ne5, ne6, ne7, ne8];
